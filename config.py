b8514f2e4195ff7aded7e8db8a026986ca3ba5c1
Denny Reiter <denny@reiters.org>
2021-02-22 17:25:15 -0600

    update mqtt server

--- config.py
+++ config.py
@@ -4,8 +4,7 @@ BED_ADDRESS = "7C:EC:79:FF:6D:02"
 
 MQTT_USERNAME = "mqttbed"
 MQTT_PASSWORD = "mqtt-bed"
-MQTT_SERVER = "lenny.homestead.kewanee.net"
-# MQTT_SERVER = "192.168.0.77"
+MQTT_SERVER = "192.168.0.77"
 MQTT_SERVER_PORT = 1883
 MQTT_TOPIC = "bed"
 
